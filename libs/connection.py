4fb62ae27ba7c1712846ce2826111434c30768d0
MartTave <martin.tavernier256@gmail.com>
2023-03-01 09:02:51 +0100

    WIP

--- libs/connection.py
+++ libs/connection.py
@@ -5,23 +5,18 @@ from enum import Enum
 encoding = "utf8"
 decoding = encoding
 
-class MessageType(Enum):
-    TEXT = 1
-    IMAGE = 2
-
 class Connection:
     started = False
     thread = None
-    recSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sendSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connected = False
     callback = None
     def __init__(self, hostname:str, port:int):
         try:
-            self.recSock.connect((hostname, port))
-            self.sendSock.connect((hostname, port))
+            self.sock.connect((hostname, port))
             self.connected = True
-        except:
+        except Exception as e:
+            print(e)
             print("Connection failed !")
     
     def startListening(self):
@@ -33,38 +28,52 @@ class Connection:
     def __listeningFunction(self):
         print("Starting listening...")
         while self.connected:
-            msg = self.recSock.recvmsg(128)
+            msg = self.sock.recvmsg(128)
             if(msg[0].decode(decoding) != ""):
-                self.callback(msg)
+                print("Received : " + msg[0].decode(decoding))
+                if(self.callback is not None):
+                    self.callback(msg)
 
     def setListeningCallback (self, callback):
         self.callback = callback
     
-    def send(self, toSend:bytes or str, type:MessageType):
+    def __printErrorMessage(self):
+        print("Please ensure that the socket is opened !")
+
+    def sendPicture(self, toSend:bytes, width:int, height:int):
+        if(not self.connected):
+            self.__printErrorMessage()
+            return
+        header = "ISCi".encode(encoding)
+        data = None
+        header += width.to_bytes(2, 'big')
+        header += height.to_bytes(2, 'big')
+        data = header + toSend
+        self.sock.send(data)
+
+    def send(self, toSend:bytes or str):
         #Protocol is 'ISC'
         # Then either i for image or t for text
         #
         # if t -> 'ISCt' + length on 2 bytes + message encoded in utf8
         # if i -> 'ISCi' + (width <= 128) + (heigh <= 128) + pixels value in RGB
-        header = "ISC".encode(encoding)
-        data = None
-        if(type == MessageType.TEXT):
-            header += 't'.encode(encoding)
-
-            # Checking data size
-            if(data.__len__ > 99):
-                print("Please send a message smaller than 100 char")
-                return
-            
-            # Converting to byte if it's a string
-            if(toSend is str):
-                toSend = toSend.encode(encoding)
-
-            # Adding length to header
-            header += toSend.__len__().to_bytes(2, 'big')
-        elif (type == MessageType.IMAGE):
-            print("Not supported yet")
+        if(not self.connected):
+            self.__printErrorMessage()
             return
+        header = "ISCt".encode(encoding)
+        data = None
+
+        # Checking data size
+        if(toSend.__len__() > 99):
+            print("Please send a message smaller than 100 char")
+            return
+        
+        # Converting to byte if it's a string
+        if(toSend is str):
+            toSend = toSend.encode(encoding)
+
+        # Adding length to header
+        header += toSend.__len__().to_bytes(2, 'big')
         message = header + toSend
-        self.sendSock.send(message)
+        self.sock.send(message)
         
--- libs/test_client.py
+++ libs/test_client.py
@@ -2,6 +2,9 @@
 
 import connection
 
+realServerIp = "vlbelintrocrypto.hevs.ch"
+realServerPort = 6000
+
 test = connection.Connection("127.0.0.1", 12345)
 test.startListening()
 test.send("Let's gooo".encode("utf8"))
