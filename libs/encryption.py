29cc3119596fb654dbd063c0545cc08605f4e72e
GabrielRossier <rossier.gabriel218@gmail.com>
2023-03-01 10:22:53 +0100

    Patch 5 bytes problem

--- libs/encryption.py
+++ libs/encryption.py
@@ -4,8 +4,10 @@ from abc import ABC, abstractmethod
 
 
 class Encrypt():
-    def formatString(self, data:str)->bytes:
+    def formatString(data:str)->bytes:
         final = ""
         for c in data:
-            final += '   ' + c
+            for x in range(4-len(c.encode("utf8"))):
+                final += '\0'
+            final += c
         return final.encode("utf8")
